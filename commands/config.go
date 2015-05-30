069ee41535673af55192855528a383f8a66c9314
Casey Kuhlman <casey@erisindustries.com>
2015-05-30 14:34:02 +0200

    add skeleton files for commands

--- commands/config.go
+++ commands/config.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/eris-ltd/eris-cli/Godeps/_workspace/src/github.com/spf13/cobra"
-	// "github.com/eris-ltd/eris-cli/Godeps/_workspace/src/github.com/spf13/viper"
 )
 
 var cmdConfig = &cobra.Command{
