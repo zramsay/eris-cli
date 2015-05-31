7b090bdb1e41201c2c7ba4a4cec9c7e2496e8b51
Casey Kuhlman <casey@erisindustries.com>
2015-05-31 19:12:29 +0200

    move config to its own package

--- commands/config.go
+++ commands/config.go
@@ -1,30 +1,55 @@
 package commands
 
 import (
-	"fmt"
+	"github.com/eris-ltd/eris-cli/config"
 
 	"github.com/eris-ltd/eris-cli/Godeps/_workspace/src/github.com/spf13/cobra"
 )
 
 var Config = &cobra.Command{
-	Use:   "config [key]:[var]",
-	Short: "Manage Configuration Settings for Eris's CLI.",
-	Long: `Display Manage configuration settings for various components of the
+	Use:   "config",
+	Short: "Manage Configuration Settings for Eris.",
+	Long: `Display and Manage configuration settings for various components of the
 Eris platform and for the platform itself.
 
 NOTE: [eris config] is only for configuring the Eris platform
 it will not work to configure any of the blockchains, services
 or projects which are managed by the Eris platform. To configure
-blockchains use [eris chains name config]; to configure services
-use [eris services name config]; to configure projects use
-[eris projects name config].`,
+blockchains use [eris chains config]; to configure services
+use [eris services config]; to configure projects use
+[eris projects config].`,
 	Run: func(cmd *cobra.Command, args []string) {
-		plopConfigVals(args)
+		// placeholder
+		config.PlopEntireConfig(globalConfig, args)
 	},
 }
 
-func plopConfigVals(args []string) {
-	for _, key := range args {
-		fmt.Println(globalConfig.GetString(key))
-	}
+// build the config subcommand
+func buildConfigCommand() {
+  Config.AddCommand(configSet)
+  Config.AddCommand(configEdit)
+}
+
+// set
+var configSet = &cobra.Command{
+  Use:   "set [key]:[var]",
+  Short: "Set a config for the Eris Platform CLI.",
+  Long:  `Set a config for the Eris Platform CLI.
+
+Note [eris config set] only operates on the settings for the eris
+cli. To set the config for a blockchain use [eris chains config]
+and to set the config for a service use [eris services config].`,
+  Run:   func(cmd *cobra.Command, args []string) {
+           config.Set(args)
+         },
+}
+
+// edit
+var configEdit = &cobra.Command{
+  Use:   "edit",
+  Short: "Edit a config for the Eris Platform CLI in an editor.",
+  Long:  `Edit a config for the Eris Platform CLI in your default editor.`,
+  Run:   func(cmd *cobra.Command, args []string) {
+           config.Edit()
+         },
 }
---
+++ config/global.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+  "fmt"
+
+  "github.com/eris-ltd/eris-cli/Godeps/_workspace/src/github.com/spf13/viper"
+)
+
+func PlopEntireConfig(globalConfig *viper.Viper, args []string) {
+  for _, key := range args {
+    fmt.Println(globalConfig.GetString(key))
+  }
+}
+
+func Set(args []string) {
+
+}
+
+func Edit() {
+
+}
