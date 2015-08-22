9d9b0152fbc364f573a371aee7b7a6c48d847ac5
Ethan Buchman <ethan@coinculture.info>
2015-08-22 23:18:36 +0000

    data: remove multiple containers at once

--- data/manage.go
+++ data/manage.go
@@ -49,32 +49,35 @@ func InspectData(do *definitions.Do) error {
 	return nil
 }
 
-func RmData(do *definitions.Do) error {
-	if util.IsDataContainer(do.Name, do.Operations.ContainerNumber) {
-		logger.Infoln("Removing data container " + do.Name)
+// XXX: what to do with error if only some (not all) are removed
+func RmData(do *definitions.Do) (err error) {
+	for _, name := range do.Args {
+		do.Name = name
+		if util.IsDataContainer(do.Name, do.Operations.ContainerNumber) {
+			logger.Infoln("Removing data container " + do.Name)
 
-		srv := definitions.BlankServiceDefinition()
-		srv.Operations.SrvContainerName = util.ContainersName("data", do.Name, do.Operations.ContainerNumber)
+			srv := definitions.BlankServiceDefinition()
+			srv.Operations.SrvContainerName = util.ContainersName("data", do.Name, do.Operations.ContainerNumber)
 
-		err := perform.DockerRemove(srv.Service, srv.Operations, false)
-		if err != nil {
-			return err
+			if err = perform.DockerRemove(srv.Service, srv.Operations, false); err != nil {
+				logger.Errorf("Error removing %s: %v", do.Name, err)
+			}
+
+		} else {
+			err = fmt.Errorf("I cannot find that data container for %s. Please check the data container name you sent me.", do.Name)
+			logger.Errorln(err)
 		}
 
-	} else {
-		return fmt.Errorf("I cannot find that data container. Please check the data container name you sent me.")
-	}
-
-	if do.RmHF {
-		logger.Println("Removing host folder " + do.Name)
-		err := os.RemoveAll(path.Join(DataContainersPath, do.Name))
-		if err != nil {
-			return err
+		if do.RmHF {
+			logger.Println("Removing host folder " + do.Name)
+			if err = os.RemoveAll(path.Join(DataContainersPath, do.Name)); err != nil {
+				return err
+			}
 		}
 	}
 
 	do.Result = "success"
-	return nil
+	return err
 }
 
 func ListKnown(do *definitions.Do) error {
