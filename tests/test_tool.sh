1a07a4dc7206fbafd413bbacdc7166926444d690
Casey Kuhlman <casey@erisindustries.com>
2015-12-06 14:08:54 +0000

    use versioned epm image; closes #329

--- tests/test_tool.sh
+++ tests/test_tool.sh
@@ -105,7 +105,7 @@ wait_procs() {
 }
 
 pull_images() {
-  images=( "quay.io/eris/base" "quay.io/eris/data" "quay.io/eris/ipfs" "quay.io/eris/keys" "quay.io/eris/erisdb:$eris_version" )
+  images=( "quay.io/eris/base" "quay.io/eris/data" "quay.io/eris/ipfs" "quay.io/eris/keys" "quay.io/eris/erisdb:$eris_version" "quay.io/eris/epm:$eris_version" )
   for im in "${images[@]}"
   do
     echo -e "Pulling image =>\t\t$im"
