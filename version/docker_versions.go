a802f3bca540c9f2b3178fd9253742723e427ece
Zach Ramsay <zach@erisindustries.com>
2016-06-06 19:52:13 -0400

    fixup versioning

---
+++ version/docker_versions.go
@@ -0,0 +1,4 @@
+package version
+
+const DOCKER_VER_MIN = "1.8.0"
+const DM_VER_MIN = "0.6.0"
--- version/version.go
+++ version/version.go
@@ -1,5 +1,3 @@
 package version
 
 const VERSION = "0.12.0"
-const DOCKER_VER_MIN = "1.8.0"
-const DM_VER_MIN = "0.6.0"
