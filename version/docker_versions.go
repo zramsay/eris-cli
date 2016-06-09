fffd51e9c63f6662b2892b0cad38c61ea51c1e0e
Zach Ramsay <zach@erisindustries.com>
2016-06-09 18:38:03 -0400

    cleaner versioning cf. eris-db

--- version/docker_versions.go
+++
@@ -1,4 +0,0 @@
-package version
-
-const DOCKER_VER_MIN = "1.8.0"
-const DM_VER_MIN = "0.6.0"
--- version/version.go
+++ version/version.go
@@ -1,3 +1,7 @@
 package version
 
+const DOCKER_VER_MIN = "1.8.0"
+const DM_VER_MIN = "0.6.0"
+// must be the last line so the build script
+// picks it up
 const VERSION = "0.12.0"
