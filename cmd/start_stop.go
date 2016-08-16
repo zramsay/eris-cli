49c38315a6a5f2ccb01d8dcc2472d65682bd1adc
slowtokyo <kyle.dupont@gmail.com>
2016-08-16 16:38:42 +0100

    WIP

--- cmd/start_stop.go
+++ cmd/start_stop.go
@@ -9,9 +9,31 @@ import (
 	. "github.com/eris-ltd/common/go/common"
 )
 
-var Start = &cobra.Command{}
+var Start = &cobra.Command{
+	Use:   "startup",
+	Short: "start up your Eris dev environment for the day",
+	Long: `start up your Eris dev environment for the day
 
-var Stop = &cobra.Command{}
+The startup command starts the services for which you have started
+in the past. The startup command also requires a '--chain' 
+flag that specifies the chain you would like to start with the
+services.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		StartEris(cmd, args)
+	},
+}
+
+var Stop = &cobra.Command{	
+	Use:   "shutdown",
+	Short: "shutdown your Eris dev environment for the day",
+	Long: `shutdown your Eris dev environment for the day
+
+The shutdown command finds all of the services and chains that 
+are currently running and shuts them down for the day. In effect,
+this command wraps:
+[eris services stop $(eris services ls -q)] and
+[eris chains stop $(eris chains ls -q)]`,
+}
 
 func buildStartCommand() {
 	addStartFlags()
--- kaihei/start.go
+++ kaihei/start.go
@@ -3,16 +3,33 @@ package kaihei
 import (
 	"fmt"
 
+//	"github.com/eris-ltd/eris-cli/chains"
+	"github.com/eris-ltd/eris-cli/util"
+	"github.com/eris-ltd/eris-cli/services"
 	"github.com/eris-ltd/eris-cli/definitions"
 )
 
 func StartUpEris(do *definitions.Do) error {
-	// list of services
-	// list of chains
-	fmt.Println("todo: start:")
 
 	// start services
+	listofServices := util.ErisContainersByType(definitions.TypeService, true)
+	names := make([]string, len(listofServices))
+	for i, serviceName := range listofServices {
+		names[i] = serviceName.ShortName
+	}
+
+	fmt.Println(names)
+
+	doStart := definitions.NowDo()
+	doStart.ServicesSlice = names
+	if err := services.StartService(doStart); err != nil {
+		return err
+	}	
 	// start chains
+	// 	IfExit(ArgCheck(1, "ge", cmd, args))
+	// do.Name = args[0]
+	// do.Run = true
+	// IfExit(chns.StartChain(do))
 	return nil
 }
 
