a8c2bb5431502cf8c09cc46735078f30fe6b3f9e
zramsay <zach@monax.io>
2016-12-12 15:11:05 -0500

    pkgs tests: add fixtures from epm

---
+++ tests/fixtures/app36-backwards_compatibility/c.sol
@@ -0,0 +1,17 @@
+contract c {
+	uint A;
+	uint B;
+	function c(uint a, uint b) {
+		A = a;
+		B = b;
+	}
+
+	function updateStorage(uint a, uint b) {
+		A = a;
+		B = b;
+	}
+
+	function getStorage() returns (uint, uint) {
+		return (A, B);
+	}
+}
