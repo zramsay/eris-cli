5eb540de2f9a9e3aee8f9dd3a79510f6665c4b49
VoR0220 <rj@erisindustries.com>
2017-01-13 20:47:16 -0600

    more verbose name used
    Signed-off-by: VoR0220 <rj@erisindustries.com>

--- util/dialogue.go
+++ util/dialogue.go
@@ -9,26 +9,26 @@ import (
 	"github.com/eris-ltd/eris-cli/log"
 )
 
-func GetStringResponse(question string, defalt string, reader *os.File) (string, error) {
+func GetStringResponse(question string, defaultAnswer string, reader *os.File) (string, error) {
 	readr := bufio.NewReader(reader)
 	log.Warn(question)
 
 	text, _ := readr.ReadString('\n')
 	text = strings.Replace(text, "\n", "", 1)
 	if text == "" {
-		return defalt, nil
+		return defaultAnswer, nil
 	}
 	return text, nil
 }
 
-func GetIntResponse(question string, defalt int, reader *os.File) (int, error) {
+func GetIntResponse(question string, defaultAnswer int, reader *os.File) (int, error) {
 	readr := bufio.NewReader(reader)
 	log.Warn(question)
 
 	text, _ := readr.ReadString('\n')
 	text = strings.Replace(text, "\n", "", 1)
 	if text == "" {
-		return defalt, nil
+		return defaultAnswer, nil
 	}
 
 	result, err := strconv.ParseInt(text, 10, 64)
@@ -41,7 +41,7 @@ func GetIntResponse(question string, defalt int, reader *os.File) (int, error) {
 
 // displays the question, scans for the response, if the response is an empty
 // string will return default, otherwise will parseBool and return the result.
-func GetBoolResponse(question string, defalt bool, reader *os.File) (bool, error) {
+func GetBoolResponse(question string, defaultAnswer bool, reader *os.File) (bool, error) {
 	var result bool
 	readr := bufio.NewReader(reader)
 	log.Warn(question)
@@ -49,7 +49,7 @@ func GetBoolResponse(question string, defalt bool, reader *os.File) (bool, error
 	text, _ := readr.ReadString('\n')
 	text = strings.Replace(text, "\n", "", 1)
 	if text == "" {
-		return defalt, nil
+		return defaultAnswer, nil
 	}
 
 	if text == "Yes" || text == "YES" || text == "Y" || text == "y" {
