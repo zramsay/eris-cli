bf8ea05ec8c47d4aee3ed7df053d33568a8c0e9b
VoR0220 <rj@erisindustries.com>
2017-01-17 13:07:31 -0600

    got rid of go-wire
    Signed-off-by: VoR0220 <rj@erisindustries.com>

--- pkgs/jobs/writers.go
+++ pkgs/jobs/writers.go
@@ -1,15 +1,10 @@
 package jobs
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
-	"reflect"
-
-	// MARMOT: dependency on go-wire needs to be removed.
-	"github.com/tendermint/go-wire"
 )
 
 const LogFileNameCSV = "epm.csv"
@@ -76,44 +71,4 @@ func setJsonPath() string {
 func setCsvPath() string {
 	pwd, _ := os.Getwd()
 	return filepath.Join(pwd, LogFileNameCSV)
-}
-
-// ------------------------------------------------------------------------
-// Writers of Arbitrary stuff
-// ------------------------------------------------------------------------
-
-// FormatOutput formats arbitrary json in a viewable manner using reflection
-func FormatOutput(args []string, i int, o interface{}) (string, error) {
-	if len(args) < i+1 {
-		return prettyPrint(o)
-	}
-	arg0 := args[i]
-	v := reflect.ValueOf(o).Elem()
-	name, err := fieldFromTag(v, arg0)
-	if err != nil {
-		return "", err
-	}
-	f := v.FieldByName(name)
-	return prettyPrint(f.Interface())
-}
-
-func fieldFromTag(v reflect.Value, field string) (string, error) {
-	iv := v.Interface()
-	st := reflect.TypeOf(iv)
-	for i := 0; i < v.NumField(); i++ {
-		tag := st.Field(i).Tag.Get("json")
-		if tag == field {
-			return st.Field(i).Name, nil
-		}
-	}
-	return "", fmt.Errorf("Invalid field name")
-}
-
-func prettyPrint(o interface{}) (string, error) {
-	var prettyJSON bytes.Buffer
-	err := json.Indent(&prettyJSON, wire.JSONBytes(o), "", "\t")
-	if err != nil {
-		return "", err
-	}
-	return string(prettyJSON.Bytes()), nil
 }
